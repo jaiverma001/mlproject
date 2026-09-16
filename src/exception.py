5a034c1b08ba865ef8e60137cc9e2bb8d1d523a5
Jai Verma <jkmalwal2@gmail.com>
2026-09-17 01:53:45 +0530

    data transformation done

--- src/exception.py
+++ src/exception.py
@@ -4,6 +4,7 @@ from src.logger import logging
 
 def error_message_detail(error, error_details):
     _, _, exc_tb = error_details.exc_info()
+
     file_name = exc_tb.tb_frame.f_code.co_filename
 
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -15,11 +16,15 @@ def error_message_detail(error, error_details):
     return error_message
 
 
-class customException(Exception):
+class CustomException(Exception):
 
     def __init__(self, error_message, error_details: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_details)
+
+        self.error_message = error_message_detail(
+            error_message,
+            error_details
+        )
 
     def __str__(self):
         return self.error_message
@@ -28,6 +33,7 @@ class customException(Exception):
 if __name__ == "__main__":
     try:
         a = 1 / 0
+
     except Exception as e:
-        logging.info("divide by zero is not possible")
-        raise customException(e, sys)
+        logging.info("Divide by zero is not possible")
+        raise CustomException(e, sys)
